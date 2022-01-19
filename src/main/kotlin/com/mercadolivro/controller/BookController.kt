cdb5d5dcdff90bba9baaa414dcf23f445d14c45b
rqlalvs <raquelalves3753@gmail.com>
2022-01-18 21:12:23 -0300

    Mercado Livro - Adicionando mais chamadas, como update, delete, pesquisa com filtro de nome

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,7 +1,9 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
@@ -19,6 +21,33 @@ class BookController(
     fun create(@RequestBody request: PostBookRequest) {
         val customer = customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel> {
+        return bookService.findAll()
+    }
+
+    @GetMapping("/active")
+    fun findActives(): List<BookModel> =
+        bookService.findActives()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel {
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        bookService.delete(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) {
+        val bookSaved = bookService.findById(id)
+        bookService.update(book.toBookModel(bookSaved))
     }
 
 }
